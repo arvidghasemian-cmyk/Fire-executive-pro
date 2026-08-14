23cdce9d773851e83fad7262d5fc2d663996ec12
arvidghasemian-cmyk <arvidghasemian@gmail.com>
2026-08-14 10:28:03 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'fire-exec-v8.2-local';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/js/three.min.js',
+  './assets/fonts/Vazirmatn-Regular.woff2',
+  './assets/fonts/Vazirmatn-Medium.woff2',
+  './assets/fonts/Vazirmatn-Bold.woff2',
+  './assets/fonts/Vazirmatn-ExtraBold.woff2'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(e.request).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
+
